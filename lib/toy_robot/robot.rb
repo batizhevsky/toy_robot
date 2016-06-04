5e668a298e412d1b4e4eab464a1708222989db00
Leonid Batizhevsky <l.batizhevsky@fun-box.ru>
2016-06-04 22:19:11 +0400

    refactor: move actions to own models

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,4 +1,72 @@
 module ToyRobot
   class Robot
+    ALLOWED_DIRECTIONS = [:north, :east, :south, :west].freeze
+
+    def initialize
+      @placed = false
+    end
+
+    def direction
+      check_placed!
+
+      @direction
+    end
+
+    def place(direction)
+      turn_on(direction)
+      @placed = true
+    end
+
+    def right
+      check_placed!
+
+      current_index = ALLOWED_DIRECTIONS.index(direction)
+      if ALLOWED_DIRECTIONS.size == current_index + 1
+        turn_on ALLOWED_DIRECTIONS.first
+      else
+        turn_on ALLOWED_DIRECTIONS[current_index + 1]
+      end
+    end
+
+    def left
+      check_placed!
+
+      current_index = ALLOWED_DIRECTIONS.index(direction)
+      if current_index == 0
+        turn_on ALLOWED_DIRECTIONS.last
+      else
+        turn_on ALLOWED_DIRECTIONS[current_index - 1]
+      end
+    end
+
+    def calculate_move(current_x, current_y)
+      check_placed!
+
+      case direction
+      when :north
+        current_x += 1
+      when :east
+        current_y += 1
+      when :south
+        current_x -= 1
+      when :west
+        current_y -= 1
+      else
+        raise 'Never will be executed'
+      end
+      [current_x, current_y]
+    end
+
+    private
+
+    def turn_on(direction)
+      raise UnknownDirection unless ALLOWED_DIRECTIONS.include?(direction)
+
+      @direction = direction
+    end
+
+    def check_placed!
+      raise RobotNotPlaced unless @placed
+    end
   end
 end
--- lib/toy_robot/table.rb
+++ lib/toy_robot/table.rb
@@ -2,9 +2,22 @@ module ToyRobot
   class Table
     def initialize(size: 5)
       @size = size
+      @x = 0
+      @y = 0
     end
 
-    def can_placed?(x,y)
+    def move_to(x, y)
+      raise OutOfTable unless can_placed?(x, y)
+      @x = x
+      @y = y
+    end
+
+    # @return [Array] return a position array with x and y coord
+    def current_position
+      [@x, @y]
+    end
+
+    def can_placed?(x, y)
       positive?(x) &&
         positive?(y) &&
         x < @size &&
